0b6715d9263acedeb38a5a06fbd314d5d61a8e04
BreyBatten <47111428+BreyBatten@users.noreply.github.com>
2019-04-18 21:10:40 -0400

    added my own comments to help me understand what is happening at each level of the file.

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -2,15 +2,19 @@ class Dropdown {
   constructor(element) {
     
     // Assign this.element to the dropdown element
+    // pull parameter from constructor function
     this.element = element;
     
     // Get the element with the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
+    // assigning button to the element of div with the class 'dropdown-button'.
     this.button = this.element.querySelector('.dropdown-button');
     
     // assign the reference to the ".dropdown-content" class found in the dropdown element
+    // assigning content to the element of div with the class of 'dropdown-content'.
     this.content = this.element.querySelector('.dropdown-content');
     
     // Add a click handler to the button reference and call the toggleContent method.
+    // taking the button, putting a click on it and calling the method below.
     this.button.addEventListener('click', () => this.toggleContent());
 
   
@@ -19,7 +23,8 @@ class Dropdown {
   toggleContent() {
     
     // Toggle the ".dropdown-hidden" class off and on
-    document.querySelector('.dropdown-content').classList.toggle('dropdown-hidden');
+    // creating the action that will be assigned to the click. Assigning the content to toggle hidden and displayed. (putting .classList means you don't need to put the "." in the 'dropdown-hidden' class)
+    this.content.classList.toggle('dropdown-hidden');
   }
 }
 
